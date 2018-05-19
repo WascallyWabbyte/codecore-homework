3a462323d9794ed59cf49ad47aba0ac8e7c9c471
Christian Fairleigh <christian-fairleigh@hotmail.com>
2018-05-18 19:50:53 -0700

    Blog on Rails

---
+++ homework5/blog_on_rails/app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
+  validates(
+    :title,
+    presence: true,
+    uniqueness: true
+  )
+  validates(
+    :body,
+    presence: true,
+    length: {
+      minimum: 16
+    }
+  )
+end
